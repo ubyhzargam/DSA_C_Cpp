6d515a4802a6b3c4292f6b1710ead8e2c69c403c
Uby H <136114596+ubyhzargam@users.noreply.github.com>
2024-11-28 15:52:59 +0530

    Create size_of_array.c

---
+++ size_of_array.c
@@ -0,0 +1,10 @@
+// Simple C code to find the size of the array and print it
+
+#include <stdio.h>
+int main()
+{
+    int A[5];
+    int s=sizeof(A);
+    printf("The size of the given array is : %d ", s);
+    return 0;
+}
